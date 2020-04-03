9dbf9555729f7cd24f3807889fc1aa5e58066ce7
ShivamShukla <33052433+shukla284@users.noreply.github.com>
2020-04-03 12:40:11 +0530

    Created a function for flipping the bits at different position.

--- Bit Manipulation/BitFlipping.cpp
+++ Bit Manipulation/BitFlipping.cpp
@@ -8,6 +8,22 @@ Author : Shivam Shukla
 Description : 
 */
 
+int64_t swapBits(int64_t n, int i, int j) {
+
+	//We will be swapping the bits iff they are different. 
+    // We can 4 possibilities at ith and jth position. (1,1), (1,0), (0,0), (0,1)
+
+    // Of these possibilities we have just to ensure the thins about (1,0) and (0, 1).
+
+    if (((n >> i) & 1) != ((n >> j) & 1)) {
+    	int64_t mask = (1 << i) | (1 << j);
+       
+        // XORing will flip the bits since xoring with 1 toggles the bit.
+        // that is why we tested the bits in the first place to change if they are different.
+    	n ^= mask;
+    }
+    return n;
+}
 
 int main(int argc,char** argv){
     
@@ -15,23 +31,8 @@ int main(int argc,char** argv){
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
 
-    int n; cin >> n; 
-    cout << bitset<8>(n) << endl;
-
-    //We will be swapping the bits iff they are different. 
-    // We can 4 possibilities at ith and jth position. (1,1), (1,0), (0,0), (0,1)
-
-    // Of these possibilities we have just to ensure the thins about (1,0) and (0, 1).
-
-    int i, j; cin >> i >> j;
-    // once the bits are taken we can extract out the bits at these positions as well.
+    int n, i, j; cin >> n >> i >> j;
+    cout << swapBits(n, i,j); 
     
-    
-    if (((n >> i) & 1) != ((n >> j) & 1)){
-    	int mask = (1 << i) | (1 << j); n ^= mask;
-    }
-
-    cout << n << endl;
-    cout << bitset<8>(n) << endl;
     return 0;		
 }
