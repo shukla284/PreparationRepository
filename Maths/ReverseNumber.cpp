655e7dc6f5b01d1606a82fb1cf3af6de1a060491
ShivamShukla <33052433+shukla284@users.noreply.github.com>
2020-04-03 18:57:54 +0530

    Added string conversion reversal

--- Maths/ReverseNumber.cpp
+++ Maths/ReverseNumber.cpp
@@ -8,6 +8,22 @@ Author : Shivam Shukla
 Description : Reverse Number (including the negative numbers)
 */
 
+int64_t reversedNumberString(int64_t n) {
+
+	int sign = (n < 0) ? -1 : +1;
+	n = abs(n);
+
+    string res = to_string(n);
+    int i = 0, j = res.length() - 1;
+
+    while (i < j) 
+    	swap(res[i], res[j]), i++, j--;
+
+    auto r = stoi(res);
+    return sign * r;
+
+}
+
 int64_t reversedNumber(int64_t n){
     //first thing is to consider about the sign of the number; then perform any operations
     // in order to determine the sign the comparison with the zeroes is enough thing to consider.
@@ -18,7 +34,7 @@ int64_t reversedNumber(int64_t n){
     while (n) 
         res = (res * 10) + (n % 10), n /= 10;
     
-    return (sign == -1) ? -res : res;
+    return sign * res;
 }
 
 int main(int argc,char** argv){
@@ -28,7 +44,7 @@ int main(int argc,char** argv){
     freopen("error.txt","w",stderr);
 
     int64_t n; cin >> n;
-    cout << reversedNumber(n) << endl;  
+    cout << reversedNumberString(n) << endl;  
     
     return 0;		
 } 
