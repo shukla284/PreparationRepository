3de6d7406cf0c8c105580a6cd8938c56c7c705a1
ShivamShukla <33052433+shukla284@users.noreply.github.com>
2020-04-09 20:19:37 +0530

    Added explanatory comments for the code

--- Array/SubarraySumEven.cpp
+++ Array/SubarraySumEven.cpp
@@ -34,11 +34,18 @@ int subarrayEvenSum_AlterNaive(vector<int>& a) {
 }
 
 int subarrayEvenSum_Efficient(vector<int>& a) {
+        // Number of remainders possible with 2 : 0, 1
+	// mod will count the even and odd numbers' occurence in the cumulative sum.
 	vector<int> mods({1,0});
+	// even numbers are initialised with 1 in order to deal with the cases in which the single element which is even can be taken into considerations
+	// In case there is no even element present in the input array then it would be giving no different answers since during handshake lemma application it will not result in significant answer.
 	int curr_sum = 0;
-
+        
 	for (int i = 0; i < a.size(); i++)
 		 curr_sum = ((curr_sum + a[i]) % 2 + 2) % 2, mods[curr_sum]++;
+	
+	// handshaking lemma is applied according to which we can have 2 choices to make from the given n things
+	// here we have to choose the starting and ending points.
 	return ((mods[0] * (mods[0] - 1)) >> 1) + ((mods[1] * (mods[1] - 1)) >> 1);	 	
 }
 
